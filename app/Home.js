a1b9778e233fdc8eea164c5099a5f4c7d8258828
Meron <119772197+WeldSD@users.noreply.github.com>
2024-04-23 23:17:11 -0600

    Pages Done
    Formatting Done!
    Links Done!

--- app/Home.js
+++ app/Home.js
@@ -1,26 +1,21 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    
     <main className="flex flex-col justify-between h-screen w-full animated-gradient">
       <div className="w-full"></div>
       <div className="text-white text-6xl font-serif mb-5">
         <p className="flex flex-col items-center justify-center flex-grow">
           Welcome to my Profile!
           <div className="flex items-center justify-center h-full">
-            <p className="text-base text-center px-52 font-serif">
-             Hello! I am currently studying at SAIT. This is a way of showcasing my work and projects. I am always looking for new opportunities and challenges. Feel free to connect with me on LinkedIn or Github.
-            
-              <p className="text-xl text-center px-82 font-serif">
-                "Meron."
-              </p>
+            <p className="text-2xl font-serif">
+              I am Meron. I am passionate about web development and I am
+              currently studying at SAIT. This is a way of showcasing my work
+              and projects. I am always looking for new opportunities and
+              challenges. Feel free to connect with me on LinkedIn or Github.
             </p>
           </div>
-      
         </p>
-     
-      
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -32,12 +27,10 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             My LinkedIn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            
-            </span>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"></span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Connect With me on LinkedIn.
+            Connect With me on LinkedIn.
           </p>
         </a>
 
@@ -49,9 +42,7 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             My Github{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              
-            </span>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"></span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             My repositories on Github!
@@ -59,20 +50,16 @@ export default function Home() {
         </a>
 
         <a
-          href="/about"
+          href=""
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             About me{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              
-            </span>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"></span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn more.
-          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Learn more.</p>
         </a>
 
         <a
@@ -83,14 +70,22 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              
-            </span>
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"></span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        <Link href="/About/about " legacyBehavior>
+          <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              About me{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"></span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Learn more.</p>
+          </a>
+        </Link>
       </div>
     </main>
   );
